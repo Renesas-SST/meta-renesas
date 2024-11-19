b5fe878ef86d91d5f363fcbcecdfa4aab5d8209c
Khanh Do Duy <khanh.do.yg@bp.renesas.com>
2024-11-19 07:12:27 +0000

    rzg2l-sbc: Support to build core-image-bsp
    Changes include:
        - Support core-image-bsp build
    
    Signed-off-by: Khanh Do Duy <khanh.do.yg@bp.renesas.com>

---
+++ recipes-extend/audio-init/audio-init/audio.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+amixer cset name='Left Input Mixer L2 Switch' on
+amixer cset name='Right Input Mixer R2 Switch' on
+amixer cset name='Headphone Playback Volume' 100
+amixer cset name='PCM Volume' 100%
+amixer cset name='Input PGA Volume' 25
