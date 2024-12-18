8b534c24316336b32147fe8752f4ab0bfb823f23
Khanh Do Duy <khanh.do.yg@bp.renesas.com>
2024-12-18 08:29:52 +0000

    rzg2l-sbc: Add gstreamer and gstreamer-omx to image
    Changes include:
        - Use Gstreamer from Yocto - version 1.24.6
        - Add Gstreamer-omx from Renesas - version 1.18.5
        - Add libomxil library that needed by gstreamer-omx
        - Add meta-rz-codecs and meta-qt5 to bblayers.conf.sample
    
    Signed-off-by: Khanh Do Duy <khanh.do.yg@bp.renesas.com>
    Signed-off-by: Tien Nguyen <tien.nguyen.uh@renesas.com>

--- recipes-extend/audio-init/audio-init/audio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-amixer cset name='Left Input Mixer L2 Switch' on
-amixer cset name='Right Input Mixer R2 Switch' on
-amixer cset name='Headphone Playback Volume' 100
-amixer cset name='PCM Volume' 100%
-amixer cset name='Input PGA Volume' 25
